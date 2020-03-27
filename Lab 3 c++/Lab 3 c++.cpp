c64fef8e44bc235e4b29e08512dc465c4bcdfbd5
RyabchenkoVova <61986198+RyabchenkoVova@users.noreply.github.com>
2020-03-27 12:38:55 +0200

    Add files via upload

---
+++ Lab 3 c++/Lab 3 c++.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <string>
+#include <stack>
+
+using namespace std;
+
+
+int main()
+{
+   string str("fg (fg gh (gh gh ) gh gh( bn bn( nm(,)))) kk (hj hj(jk m(jk m))) mm");
+   stack<char> stk;
+    for (int i = 0; i < str.size(); ++i)
+    {
+        if ('(' == str[i])
+        {
+            stk.push(i);
+        }
+        else if (')' == str[i])
+        {
+            int ind = stk.top();
+            stk.pop();
+            str.erase(ind, i - ind + 1);
+            i = ind - 1;
+        }
+    }
+   cout << str << std::endl;
+}
+ 
+
